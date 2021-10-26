c62c254eaa1c5ce9073993e71469851276c10722
Pierre Hauweele <phauweele@he2b.be>
2021-10-26 09:02:18 +0200

    Add sol exo 78: primeFactors

--- exemples/ExosIteratifs.java
+++ exemples/ExosIteratifs.java
@@ -22,9 +22,51 @@ public class ExosIteratifs {
         return somme;
     }
 
+    /**
+     * Résolution de l'exo 78
+     */
+    public static void decompoFacteursPremiers(int nombre) {
+        // 1001880
+        // 2 2 2 3 3 5 11 11 23
+        // 2 * 2 * 2 * 3 * 3 * 5 * 11 * 11 * 23
+        // 2^3 * 3^2 * 5^1 * 11^2 * 23^1
+        // 2^3 * 3^2 * 5 * 11^2 * 23
+        int facteur = 2;
+        int exposant = 0;
+        while (nombre > 1) {
+            if (nombre % facteur == 0) {
+                nombre /= facteur;
+                ++exposant;
+            } else {
+                switch (exposant) {
+                    case 0:
+                        break;
+                    case 1:
+                        System.out.print(facteur + " * ");
+                        break;
+                    default:
+                        System.out.print(
+                                facteur + "^" + exposant + " * ");
+                }
+                exposant = 0;
+                ++facteur;
+            }
+        }
+
+        if (exposant > 0) {
+            System.out.print(facteur);
+        }
+        if (exposant > 1) {
+            System.out.print("^" + exposant);
+        }
+
+        System.out.println("");
+    }
+
     public static void main(String[] args) {
         int nombre = Integer.valueOf(args[0]);
-        System.out.println(sommeChiffresTriche(nombre));
-        System.out.println(sommeChiffres(nombre));
+        decompoFacteursPremiers(nombre);
+        //System.out.println(sommeChiffresTriche(nombre));
+        //System.out.println(sommeChiffres(nombre));
     }
 }
